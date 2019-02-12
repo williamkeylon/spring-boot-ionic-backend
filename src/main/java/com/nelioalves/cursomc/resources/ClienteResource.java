7682e48c8db0da54356c8d73db7e52f5a460b080
William Keylon <williamgoncalves@msn.com>
2019-02-11 22:20:34 -0300

    Endpoint /cliendes/{id} disponivel

---
+++ src/main/java/com/nelioalves/cursomc/resources/ClienteResource.java
@@ -0,0 +1,27 @@
+package com.nelioalves.cursomc.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.nelioalves.cursomc.domain.Cliente;
+import com.nelioalves.cursomc.services.ClienteService;
+
+@RestController
+@RequestMapping("/clientes")
+public class ClienteResource {
+
+	@Autowired
+	private ClienteService service;
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<?> find(@PathVariable Integer id) {		
+		Cliente obj = service.buscar(id);				
+		return ResponseEntity.status(HttpStatus.OK).body(obj);
+	}
+	
+}
