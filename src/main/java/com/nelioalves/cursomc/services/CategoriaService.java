246383d6b71867f9f77a81eab97481dfa45c9de4
William Keylon <williamgoncalves@msn.com>
2019-02-17 23:34:14 -0300

    Inserindo uma nova Categoria com POST

--- src/main/java/com/nelioalves/cursomc/services/CategoriaService.java
+++ src/main/java/com/nelioalves/cursomc/services/CategoriaService.java
@@ -20,4 +20,9 @@ public class CategoriaService {
 				"Recurso nao Encontrado! Id: "+id+", Tipo: "+Categoria.class.getName()));
 	}
 	
+	
+	public Categoria insert(Categoria obj) {
+		obj.setId(null);
+		return repo.save(obj);
+	}
 }
